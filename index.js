4548817100e0c765c73bce90956a81b4e5cb2f67
Kamran Hussein <chinterswill@gmail.com>
2022-11-01 11:33:24 -0400

    commit tree 9559cb5abe8efbe7008de04db0d84c4cc64986d0

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+const event = "surprise"
+const nameEvent = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        nameEvent.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return nameEvent;
+}
+
+let num = 13;
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num--);
+    }
+}
+
+countDown(num)
